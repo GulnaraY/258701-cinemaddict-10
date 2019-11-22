67ebec75c867ded32341987ec35d5eb52e102bc7
GulnaraY <guga.ufa@gmail.com>
2019-11-22 23:38:15 +0300

    исправляет табуляцию в модулях, убирает лишние строки и выносит повторяющийся код в переменные

--- src/components/films-container.js
+++ src/components/films-container.js
@@ -8,23 +8,23 @@ const FILM_COUNT_MOST_COMMENTED = 2;
 export const createFilmsContainer = () => {
   return (`<section class="films">
   <section class="films-list">
-  <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-  <div class="films-list__container">
-  ${new Array(FILM_COUNT_MAIN).fill(``).map(() => createFilmCard()).join(``)}
-  </div>
-  ${createShowMoreButton()}
+    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+    <div class="films-list__container">
+      ${new Array(FILM_COUNT_MAIN).fill(``).map(() => createFilmCard()).join(``)}
+    </div>
+    ${createShowMoreButton()}
   </section>
   <section class="films-list--extra">
-  <h2 class="films-list__title">Top rated</h2>
-  <div class="films-list__container">
-  ${new Array(FILM_COUNT_TOP_RATED).fill(``).map(() => createFilmCard()).join(``)}
-  </div>
+    <h2 class="films-list__title">Top rated</h2>
+    <div class="films-list__container">
+      ${new Array(FILM_COUNT_TOP_RATED).fill(``).map(() => createFilmCard()).join(``)}
+    </div>
   </section>
   <section class="films-list--extra">
-  <h2 class="films-list__title">Most commented</h2>
-  <div class="films-list__container">
-  ${new Array(FILM_COUNT_MOST_COMMENTED).fill(``).map(() => createFilmCard()).join(``)}
-  </div>
+    <h2 class="films-list__title">Most commented</h2>
+    <div class="films-list__container">
+      ${new Array(FILM_COUNT_MOST_COMMENTED).fill(``).map(() => createFilmCard()).join(``)}
+    </div>
   </section>
   </section>`);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 
 const path = require(`path`);
+const outputPath = path.join(__dirname, `public`);
 
 module.exports = {
   mode: `development`,
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`),
+    path: outputPath,
   },
   devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, `public`),
+    contentBase: outputPath,
     publicPath: 'http://localhost:8080/',
     compress: true,
     watchContentBase: true
