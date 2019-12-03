16f0cb18c231c7d3e2433c313e386a1bf33c2e35
GulnaraY <guga.ufa@gmail.com>
2019-12-03 11:05:48 +0300

    удаляем дублирование данных в модуле data.js и film-data.js

--- src/mock/film-data.js
+++ src/mock/film-data.js
@@ -1,4 +1,5 @@
 import {getRandomBoolean, getRandomElement, getRandomNumber, getRandomPart, getRandomFloatNumber} from '../util.js';
+import {titles, descriptionParts, posters, genres} from './data.js';
 
 const DESCRIPTION_MIN = 1;
 const DESCRIPTION_MAX = 3;
@@ -8,56 +9,6 @@ const LATEST_YEAR = 2019;
 const MIN_DURATION = 20;
 const MAX_DURATION = 180;
 const MAX_COMMENTS_QUANTITY = 5;
-
-const titles = new Set([
-  `Friends`,
-  `The Big Bang Theory`,
-  `Revolutionary Road`,
-  `The social network`,
-  `The green mile`,
-  `The Great Gatsby`,
-  `Star Wars`,
-  `Leon`,
-  `Game of Trones`,
-  `The Matrix`,
-  `Forrest Gamp`,
-  `A beautiful mind`,
-  `Harry Potter`,
-  `Frozen`,
-  `Lalaland`,
-]);
-
-const descriptionParts = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-  `Fusce tristique felis at fermentum pharetra.`,
-  `Aliquam id orci ut lectus varius viverra.`,
-  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-  `Aliquam erat volutpat.`,
-  `Nunc fermentum tortor ac porta dapibus.`,
-  `In rutrum ac purus sit amet tempus.`,
-];
-
-const posters = [
-  `made-for-each-other.png`,
-  `popeye-meets-sinbad.png`,
-  `sagebrush-trail.jpg`,
-  `santa-claus-conquers-the-martians.jpg`,
-  `the-dance-of-life.jpg`,
-  `the-great-flamarion.jpg`,
-  `the-man-with-the-golden-arm.jpg`,
-];
-
-const genres = new Set([
-  `comedy`,
-  `drama`,
-  `cartoon`,
-  `horror`,
-  `TVshow`,
-]);
 
 export const generateFilm = () => {
   return {
