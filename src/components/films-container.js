f942e4df7f5699dd69ecc3604ebc9dde05c0e398
Gulnara <guga.ufa@gmail.com>
2019-12-09 14:07:28 +0300

    метод crateFilmsContainer делает приватным методом _createFilmsContainer класса FilmsContainer

--- src/components/films-container.js
+++ src/components/films-container.js
@@ -3,28 +3,6 @@
 import {createElement} from '../util.js';
 
 /**
- * Метод возвращающий верстку контейнера фильмов
- * @param {Class} showMoreButton - экземпляр класса ShowMoreButton
- * @param {Object} sortingMap - мапа для сортировки
- * @return {String}- разметка
- */
-const createFilmsContainer = (showMoreButton, sortingMap) => {
-  return (`<section class="films">
-  <section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    <div class="films-list__container">
-    </div>
-     ${showMoreButton.getTemplate()}
-  </section>
-   ${Object.keys(sortingMap).map((sorter) => (`<section class="films-list--extra">
-    <h2 class="films-list__title">${sortingMap[sorter]}</h2>
-    <div class="films-list__container">
-    </div>
-  </section>`)).join(``)}
-  </section>`);
-};
-
-/**
  *Класс для создания компонента контейнера фильмов
 принимает на вход ссылку на созданный компонент showMoreButton
  */
@@ -38,8 +16,24 @@ export default class Filmscontainer {
     };
   }
 
+  _createFilmsContainer() {
+    return (`<section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      <div class="films-list__container">
+      </div>
+       ${this._showMoreButton.getTemplate()}
+    </section>
+     ${Object.keys(this._sortingMap).map((sorter) => (`<section class="films-list--extra">
+      <h2 class="films-list__title">${this._sortingMap[sorter]}</h2>
+      <div class="films-list__container">
+      </div>
+    </section>`)).join(``)}
+    </section>`);
+  }
+
   getTemplate() {
-    return createFilmsContainer(this._showMoreButton, this._sortingMap);
+    return this._createFilmsContainer(this._showMoreButton, this._sortingMap);
   }
 
   getElement() {
