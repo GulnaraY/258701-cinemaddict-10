ac1d0ee6e60f364026d33dd617094bde0ff8074c
Gulnara <guga.ufa@gmail.com>
2019-12-17 10:54:33 +0300

    добавляет дата-атрибуты к сортировке

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -6,9 +6,9 @@ export default class Sorting extends AbstractComponent {
 
   getTemplate() {
     return `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort-type="default" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" data-sort-type="date" class="sort__button">Sort by date</a></li>
+      <li><a href="#" data-sort-type="rating" class="sort__button">Sort by rating</a></li>
     </ul>`;
   }
 }
