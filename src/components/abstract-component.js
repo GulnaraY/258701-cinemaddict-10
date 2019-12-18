530ba6b2a82a0a4fe7df0b5251fcb18f94389699
Gulnara <guga.ufa@gmail.com>
2019-12-18 13:29:30 +0300

    добавляет документирование jsdoc

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -1,5 +1,12 @@
+/**
+ * Модуль для абстрактного класса AbstractComponent
+ */
+
 import {createElement} from '../utils/render.js';
 
+/**
+ * Абстрактный класс, от которого наследуются компоненты
+ */
 export default class AbstractComponent {
   constructor() {
     if (new.target === AbstractComponent) {
@@ -9,10 +16,19 @@ export default class AbstractComponent {
     this._element = null;
   }
 
+  /**
+   * @public
+   * метод изменяется у потомков, использовать нельзя
+   */
   getTemplate() {
     throw new Error(`Abstract method not implemented: getTemplate`);
   }
 
+  /**
+   * @public
+   * формирует элемент на основе разметки
+   * @return {String}
+   */
   getElement() {
     if (!this._element) {
       this._element = createElement(this.getTemplate());
@@ -21,6 +37,10 @@ export default class AbstractComponent {
     return this._element;
   }
 
+  /**
+   * @public
+   * Удаляет ссылку на элемент
+   */
   removeElement() {
     this._element = null;
   }
--- src/components/no-films.js
+++ src/components/no-films.js
@@ -1,5 +1,14 @@
+/**
+ * Модуль для генерации экземпляра класса для создания
+ * компонент для отрисовки при отсутствии данных
+ */
+
 import AbstractComponent from './abstract-component.js';
 
+/**
+ * Класс для создания компонента для отрисовки
+ * при отсутствии данных
+ */
 export default class NoFilms extends AbstractComponent {
   getTemplate() {
     return (`<section class="films">
