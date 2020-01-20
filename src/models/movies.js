e5fbc2faec0e65be160cb85ab8de593272c91e0b
Gulnara <guga.ufa@gmail.com>
2020-01-20 15:34:00 +0300

    добавляет методы в movies

--- src/models/movies.js
+++ src/models/movies.js
@@ -1,3 +1,21 @@
 export default class Movies {
+  constructor() {
+    this._movies = [];
+  }
 
+  getMovies() {
+
+  }
+
+  getMoviesAll() {
+    return this._movies;
+  }
+
+  setMovies(movies) {
+    this._movies = Array.from(movies);
+  }
+
+  updateMovie(movieId, movie) {
+
+  }
 }
